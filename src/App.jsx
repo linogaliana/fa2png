c0e301e3e20629c65b7083333d95dd5b53dc043b
lgaliana <lino.galiana@insee.fr>
2025-04-02 09:46:23 +0000

    ;

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import Nav from './components/Nav';
 import Container from './components/Container';
 import Footer from './components/Footer';
